14c231f336f324a974d5f221c265d108ad91e51e
Appleeyes <appleeyespackage@gmail.com>
2024-06-09 00:10:39 +0100

    update swagger for deployment

--- scripts/00-laravel-deploy.sh
+++ scripts/00-laravel-deploy.sh
@@ -3,11 +3,23 @@ echo "Running composer"
 composer global require hirak/prestissimo
 composer install --no-dev --working-dir=/var/www/html
 
+echo "Publishing Swagger UI assets..."
+php artisan vendor:publish --provider "L5Swagger\L5SwaggerServiceProvider"
+
+echo "Generating Swagger documentation..."
+php artisan l5-swagger:generate
+
 echo "Caching config..."
 php artisan config:cache
 
 echo "Caching routes..."
 php artisan route:cache
+
+echo "Clearing view cache..."
+php artisan view:clear
+
+echo "Clearing application cache..."
+php artisan cache:clear
 
 echo "Running migrations..."
 php artisan migrate --force
