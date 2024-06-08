971beb21c4a9b0de94ba27a1567372d44a661d35
Appleeyes <appleeyespackage@gmail.com>
2024-06-08 23:54:09 +0100

    set up swagger documentation

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -6,6 +6,13 @@ use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
 
+/**
+ * @OA\Info(
+ *      title="MyHome.NG",
+ *      version="1.0.0",
+ *      description="The MyHome.ng project is a web application designed to streamline the process of finding, renting, and managing properties.",
+ * )
+ */
 class Controller extends BaseController
 {
     use AuthorizesRequests, ValidatesRequests;
