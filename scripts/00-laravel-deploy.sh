f99c86ee21522805ceea71cd672f406822dfb5fe
Appleeyes <appleeyespackage@gmail.com>
2024-06-09 02:28:39 +0100

    added route to web app for deploy 6

--- scripts/00-laravel-deploy.sh
+++ scripts/00-laravel-deploy.sh
@@ -1,23 +1,7 @@
 #!/usr/bin/env bash
-
 echo "Running composer"
 composer global require hirak/prestissimo
 composer install --no-dev --working-dir=/var/www/html
-
-echo "Publishing Swagger assets..."
-php artisan vendor:publish --provider "L5Swagger\L5SwaggerServiceProvider"
-
-echo "Creating directory for Swagger assets..."
-mkdir -p /var/www/html/public/vendor/l5-swagger
-
-echo "Copying Swagger assets..."
-cp -r /var/www/html/vendor/darkaonline/l5-swagger/public/* /var/www/html/public/vendor/l5-swagger/
-
-echo "Listing contents of /var/www/html/public"
-ls -l /var/www/html/public
-
-echo "Listing contents of /var/www/html/public/vendor/l5-swagger"
-ls -l /var/www/html/public/vendor/l5-swagger
 
 echo "Caching config..."
 php artisan config:cache
