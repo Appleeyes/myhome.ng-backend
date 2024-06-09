227ca9be428a257c5f6a5bc00bd06c2312268f07
Appleeyes <appleeyespackage@gmail.com>
2024-06-09 01:30:58 +0100

    added route to web app for deploy 2

--- routes/web.php
+++ routes/web.php
@@ -16,7 +16,3 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-
-Route::get('api/documentation', function () {
-    return view('vendor.l5-swagger.index');
-});
