43e9ac993e2de3bc3e2a838547d35cfc9d3bf31d
Appleeyes <appleeyespackage@gmail.com>
2024-06-11 22:00:19 +0100

    continuous developement and integration

--- scripts/00-laravel-deploy.sh
+++ scripts/00-laravel-deploy.sh
@@ -6,11 +6,11 @@ composer install --no-dev --working-dir=/var/www/html
 # echo "generating application key..."
 # php artisan key:generate --show
 
-echo "Creating public/docs directory if it doesn't exist..."
-mkdir -p /var/www/html/public/docs
+# echo "Creating public/docs directory if it doesn't exist..."
+# mkdir -p /var/www/html/public/docs
 
-echo "Creating symbolic link for Swagger UI assets..."
-ln -s /var/www/html/vendor/swagger-api/swagger-ui/dist/ /var/www/html/public/docs/asset
+# echo "Creating symbolic link for Swagger UI assets..."
+# ln -s /var/www/html/vendor/swagger-api/swagger-ui/dist/ /var/www/html/public/docs/asset
 
 echo "Caching config..."
 php artisan config:cache
